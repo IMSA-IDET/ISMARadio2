d1a33245edfa87e2b12f3e7035461748cd54d263
nekiwo <58634064+nekiwo@users.noreply.github.com>
2022-12-12 00:16:08 -0600

    ui

--- public/js/audio.js
+++ public/js/audio.js
@@ -1,52 +1,56 @@
 // Config
 const serverURL = "ws://localhost:3001";
 
-const playBtn = document.getElementsByTagName("button")[0]//document.getElementById("playButton");
+const playBtn = document.getElementById("playButton");
+const playBtnImage = document.getElementById("playButtonImage");
+const pauseBtnImage = document.getElementById("pauseButtonImage");
+
 let playClicked = false;
 
-let context = new (window.AudioContext || window.webkitAudioContext)();
+let context = null;
 let socket = new WebSocket(serverURL);
 
 let volume = 1;
 
-playBtn.addEventListener("click", _ => {
+playBtn.addEventListener("click", () => {
     if (!playClicked) {
         playClicked = true;
 
-        // TODO: do some button effects/animations/style changes
-        playBtn.innerHTML = "pause";
+        context = new (window.AudioContext || window.webkitAudioContext)();
 
-        socket.addEventListener("message", async event => {
-            let socketBuffer = await event.data.arrayBuffer();
-            socketBuffer = new Int32Array(socketBuffer);
-
-            let visualBuffer = new Float32Array(socketBuffer.length);
-
-            const audioBuffer = context.createBuffer(
-                1,
-                44100,
-                44100
-            );
-
-            for (let i = 0; i < audioBuffer.length; i++) {
-                visualBuffer[i] = socketBuffer[i] / 2147483647; // 2^32 / 2 -1
-                audioBuffer.getChannelData(0)[i] = visualBuffer[i] * volume;
-            }
-
-            // TODO: send socketBuffer array to canvas audio visual
-
-            const source = context.createBufferSource();
-            source.buffer = audioBuffer;
-            source.connect(context.destination);
-            source.start();
-        });
+        playBtnImage.style.display = "none";
+        pauseBtnImage.style.display = "block";
     } else {
         playClicked = false;
 
-        context = null;
-        socket = null;
-
-        // TODO: do some button effects/animations/style changes
-        playBtn.innerHTML = "play";
+        playBtnImage.style.display = "block";
+        pauseBtnImage.style.display = "none";
     }
 })
+
+socket.addEventListener("message", async event => {
+    if (playClicked) {
+        let socketBuffer = await event.data.arrayBuffer();
+        socketBuffer = new Int32Array(socketBuffer);
+
+        let visualBuffer = new Float32Array(socketBuffer.length);
+
+        const audioBuffer = context.createBuffer(
+            1,
+            44100,
+            44100
+        );
+
+        for (let i = 0; i < audioBuffer.length; i++) {
+            visualBuffer[i] = socketBuffer[i] / 2147483647; // 2^32 / 2 -1
+            audioBuffer.getChannelData(0)[i] = visualBuffer[i] * volume;
+        }
+
+        // TODO: send socketBuffer array to canvas audio visual
+
+        const source = context.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(context.destination);
+        source.start();
+    }
+});
--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,6 @@
 
 // Volume slider lower range background
 let volumeSlider = document.getElementById("volcontrol");
-console.log(volumeSlider)
 volumeSlider.addEventListener("input", e => {
     volumeSlider.style.background = `linear-gradient(to right, var(--iconcolor) 0%, var(--iconcolor) ${volumeSlider.value}%, #000 ${volumeSlider.value}%, #000 100%)`;
     volume = volumeSlider.value;

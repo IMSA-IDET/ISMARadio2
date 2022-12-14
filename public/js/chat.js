6873478d820c4c4a4e88362e09f6c77653ac9182
nekiwo <58634064+nekiwo@users.noreply.github.com>
2022-12-14 16:09:32 -0600

    string to color

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,34 +1,47 @@
 const chatServerUrl = `ws://${window.location.hostname}:${window.location.port}/?path=chat`;
 
-const randomNameColor = Math.floor(Math.random()*16777215).toString(16);
+const getColorFromName = name => {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = "#";
+    for (let i = 0; i < 3; i++) {
+        let value = (hash >> (i * 8)) & 0xFF;
+        color += ("00" + value.toString(16)).substr(-2);
+    }
+    
+    return color;
+}
 let serversocket = new WebSocket(chatServerUrl);
 
 
 function sendChatMessage(username, message) {
-    const container = document.createElement('div'); container.classList.add('chatMessage');
-    const name = document.createElement('div'); name.classList.add('chatSender');
+    const container = document.createElement("div"); container.classList.add("chatMessage");
+    const name = document.createElement("div"); name.classList.add("chatSender");
     container.append(name)
-    const msg = document.createElement('div'); msg.classList.add('chatContent')
+    const msg = document.createElement("div"); msg.classList.add("chatContent")
     container.append(msg)
-    name.innerText = username+ ": ";
+    name.innerText = username + ": ";
     msg.innerText = message;
     name.style.color = randomNameColor
-    document.getElementById('chat').append(container)
+    document.getElementById("chat").append(container)
 
 }
 
 serversocket.addEventListener("message", async event => {
     var data = await event.data.text()
     var jsondata = JSON.parse(data)
-    sendChatMessage(jsondata.name,jsondata.message)
+    sendChatMessage(jsondata.name, jsondata.message)
 })
 
-const sendmessageHandle = ()=>{
-    if (document.getElementById('chatInput').value == "") return false
-    serversocket.send(JSON.stringify({'name':"joe biben","message": document.getElementById('chatInput').value}))
-    document.getElementById('chatInput').value = ""
+const sendmessageHandle = () => {
+    if (document.getElementById("chatInput").value == "") return false
+    serversocket.send(JSON.stringify({ "name": "joe biben", "message": document.getElementById("chatInput").value }))
+    document.getElementById("chatInput").value = ""
 }
-document.getElementById('msgsend').addEventListener("click", sendmessageHandle);
+document.getElementById("msgsend").addEventListener("click", sendmessageHandle);
 
 
-document.onkeyup = (key)=>{ if (key.key == "Enter") sendmessageHandle();}
+document.onkeyup = (key) => { if (key.key == "Enter") sendmessageHandle(); }

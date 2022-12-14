ab48d0c9c4195b782336d2c2178dc71440c57675
nekiwo <58634064+nekiwo@users.noreply.github.com>
2022-12-14 12:43:13 -0600

    server stuff

---
+++ server/db.js
@@ -0,0 +1,7 @@
+import fs from "fs"
+
+export const GetPassword = (password) => {
+    fs.readFile("db/auth.json", (data) => {
+        confirm
+    })
+}
--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 import express from "express"
-import { Server } from "socket.io"
 import http from "http"
 import bodyParser from "body-parser"
-import fs from "fs"
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { WebSocketServer } from "ws"
 
 // Config
 const PORT = 3001;
@@ -12,15 +9,24 @@ const PORT = 3001;
 // App Setup
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-app.use(express.static("public", { extensions: ["html"] }))
-app.use(bodyParser.urlencoded({ extended: true }))
+const wss = new WebSocketServer({ server: server, path: "/stream" });
 
+app.use(express.static("public", { extensions: ["html"] }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
+// Websocket proxy
+wss.on("connection", ws => {
+    const parameters = url.parse(req.url, true);
+    if (GetPassword(parameters.query.password)) {
+        ws.on("message", data => {
+            wss.clients.forEach(client => {
+                client.send(data);
+            });
+        });
+    }
+});
 
 //Start server listening
 server.listen(PORT, () => {
-    console.log("Server listening on port:", PORT)
-})
+    console.log("Server listening on port:", PORT);
+});
